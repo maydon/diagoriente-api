b3be1c501a3b4d97e735216d476426f184deee26
ben alaya safouene <benalaya.safouene@gmail.com>
2019-02-21 15:24:19 +0100

    added prod config

--- src/config/vars.js
+++ src/config/vars.js
@@ -6,22 +6,18 @@ require('dotenv-safe').load({
   sample: path.join(__dirname, '../../.env.example')
 });
 
+let ext = '';
+if (process.env.NODE_ENV === 'production') ext = '_PROD';
+else if (process.env.NODE_ENV === 'test') ext = '_TESTS';
+
 module.exports = {
-  serverUrl:
-    process.env.NODE_ENV === 'test'
-      ? process.env.SERVER_URL_TESTS
-      : process.env.SERVER_URL,
+  serverUrl: process.env[`SERVER_URL${ext}`],
   env: process.env.NODE_ENV,
-  port: process.env.NODE_ENV === 'test'
-  ? process.env.PORT_TESTS
-  : process.env.PORT,
+  port: process.env[`PORT${ext}`],
   jwtSecret: process.env.JWT_SECRET,
   jwtExpirationInterval: process.env.JWT_EXPIRATION_MINUTES,
   mongo: {
-    uri:
-      process.env.NODE_ENV === 'test'
-        ? process.env.MONGO_URI_TESTS
-        : process.env.MONGO_URI
+    uri: process.env[`MONGO_URI${ext}`]
   },
   logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev'
 };
