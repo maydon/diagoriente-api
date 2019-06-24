c313a040c40c640493c54691c2ce7e8dafcff4cd
ben alaya safouene <benalaya.safouene@gmail.com>
2019-06-24 17:05:07 +0100

    changed uploads location

--- src/api/routes/v1/index.js
+++ src/api/routes/v1/index.js
@@ -28,7 +28,7 @@ router.get('/status', (req, res) => res.send('OK'));
  */
 
 router.use('/docs', express.static(path.join(__dirname, '../../../../docs')));
-router.use('/icons', express.static(path.join(__dirname, '../../../uploads')));
+router.use('/icons', express.static(path.join(__dirname, '../../../../uploads')));
 router.use('/assets', express.static(path.join(__dirname, '../../../assets')));
 
 router.use('/auth', authRoutes);
