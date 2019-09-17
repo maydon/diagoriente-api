02ba102d054e2de62bb779ed9b11321882b19a4d
ben alaya safouene <benalaya.safouene@gmail.com>
2019-09-17 19:40:26 +0100

    fix allow codeGroupe

--- src/api/validations/user.validation.js
+++ src/api/validations/user.validation.js
@@ -127,7 +127,7 @@ module.exports = {
       lastName: Joi.string().max(30),
       pseudo: Joi.string().max(25),
       institution: Joi.string().max(70),
-      codeGroupe: Joi.string(),
+      codeGroupe: Joi.string().allow(''),
       tutorial: Joi.array(),
       question: {
         _id: Joi.string().required(),
